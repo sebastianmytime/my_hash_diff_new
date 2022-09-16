f143a4681d96745b6a5a10c78bf33e397ad416a6
sebastian_lascau <sebastian@mytime.com>
2022-09-16 13:37:29 +0300

    update

--- lib/my_hash_diff_new.rb
+++ lib/my_hash_diff_new.rb
@@ -1,12 +1,19 @@
-# my diff implementation using delete_if
+# hash diff using active support
+require 'active_support'
 
 module SebastianHash
   refine Hash do
     def my_diff(hash)
-      hash.each do |key, value|
-        delete_if { |k, v| k == key and v == value }
-      end
-      self
+      self.except(*hash.keys)
     end
   end
 end
+
+using SebastianHash
+
+puts({a:1, b:2, c:3}.my_diff({c:3, b:2}))
+
+
+hash = { a: 1, b: 2, c: 3}
+puts(hash.except(:c))     # => { a: true, b: false }
+puts(hash.except(:a, :b)) # => { c: nil }
